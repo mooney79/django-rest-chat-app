2d794d85bffd898af980676d0b51f0881a95c9a3
Samuel Mooney <lappletop@Samuels-MacBook-Pro.local>
2021-10-13 12:33:15 -0400

    another typo

--- conf/settings.py
+++ conf/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',
-                'django-chat-app-mooney79.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-chat-app-mooney79.herokuapp.com'
+                ]
 
 
 # Application definition
